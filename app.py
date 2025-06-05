a2bb3ae38ef5c4956db1b6ad0be91dfeb8f021a3
Sambi <sambisingh289@gmail.com>
2025-06-05 15:33:41 -0500

    App wont suddenly end now and added method headers

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
 
+def inToCm(input):
+    pass
+def ftToMeter(input):
+    pass
+def lbToKilogram(input):
+    pass
+
 def startMsg():
     units = {'in': 'cm', 'ft':'m', 'lb':'kg'}
     
-    print("**-- Please enter the abbreviation (in, ft, lb) of the unit you wish to convert --**\n")
+    print("**-- Please enter the abbreviation (in, ft, lb, and q to quit) of the unit you wish to convert --**\n")
     for unit in units:
         print(f"{unit} to {units[unit]}")
     print("---------------------")
 def main():
     #print("hi")
-    startMsg()
-    inputVal = input()
-    if inputVal.lower() == 'in':
-        pass
-    if inputVal.lower() == 'lb':
-        pass
+    while(True):
+        startMsg()
+        inputVal = input()
+        if inputVal.lower() == 'in':
+            pass
+        elif inputVal.lower() == 'ft':
+            pass
+        elif inputVal.lower() == 'lb':
+            pass
+        elif inputVal.lower() == 'q':
+            print("Qutting now, bye bye :)")
+            break
+        else:
+            print("Error please enter either in, ft, or lb")
+
+
 
 if __name__ == '__main__':
     main()
